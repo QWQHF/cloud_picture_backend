b4ee9658ae6b7daca25e23eca7996c1b610193d0
HF <3123961845@qq.com>
2025-07-06 21:58:30 +0800

    图片管理功能

---
+++ src/main/java/com/hf/cloudpicture/mapper/PictureMapper.java
@@ -0,0 +1,18 @@
+package com.hf.cloudpicture.mapper;
+
+import com.hf.cloudpicture.modle.entity.Picture;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author HF
+* @description 针对表【picture(图片)】的数据库操作Mapper
+* @createDate 2025-07-06 16:43:16
+* @Entity com.hf.cloudpicture.modle.entity.Picture
+*/
+public interface PictureMapper extends BaseMapper<Picture> {
+
+}
+
+
+
+
---
+++ src/main/java/com/hf/cloudpicture/service/PictureService.java
@@ -0,0 +1,58 @@
+package com.hf.cloudpicture.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.hf.cloudpicture.modle.dto.picture.PictureQueryRequest;
+import com.hf.cloudpicture.modle.dto.picture.PictureUploadRequest;
+import com.hf.cloudpicture.modle.entity.Picture;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.hf.cloudpicture.modle.entity.User;
+import com.hf.cloudpicture.modle.vo.PictureVO;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+* @author HF
+* @description 针对表【picture(图片)】的数据库操作Service
+* @createDate 2025-07-06 16:43:16
+*/
+public interface PictureService extends IService<Picture> {
+
+    /**
+     * 上传图片
+     * @param multipartFile
+     * @param loginUser
+     * @return
+     */
+    PictureVO uploadPicture(MultipartFile multipartFile, PictureUploadRequest pictureUploadRequest, User loginUser);
+
+    /**
+     * 获取查询对象
+     * @param pictureQueryRequest
+     * @return
+     */
+    QueryWrapper<Picture> getQueryWrapper(PictureQueryRequest pictureQueryRequest);
+
+    /**
+     * 获取封装类
+     * @param picture
+     * @param request
+     * @return
+     */
+    PictureVO getPictureVO(Picture picture, HttpServletRequest request);
+
+    /**
+     * 分页获取图片封装类
+     * @param picturePage
+     * @param request
+     * @return
+     */
+    Page<PictureVO> getPictureVOPage(Page<Picture> picturePage, HttpServletRequest request);
+
+    /**
+     * 校验
+     * @param picture
+     */
+    void validPicture(Picture picture);
+}
